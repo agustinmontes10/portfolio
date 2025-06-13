36929cd14e4e4fb1059db384e3f70d94a92627cb
agustinmontes10 <montesagustin99@gmail.com>
2025-06-13 15:20:19 -0300

    fixes for deploy

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  eslint: {
-    ignoreDuringBuilds: true, // 🔥 Esto ignora errores de ESLint en el build
-  },
 };
 
 export default nextConfig;
